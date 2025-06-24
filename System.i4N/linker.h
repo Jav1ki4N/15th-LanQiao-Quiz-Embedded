aa47c588a0f059d269b09fe2296b8a46889856dc
Jav1ki4N <151114367+Jav1ki4N@users.noreply.github.com>
2025-06-24 17:57:23 +0800

    Add files via upload

---
+++ System.i4N/linker.h
@@ -0,0 +1,27 @@
+#ifndef _LINKER_H
+#define _LINKER_H
+
+//basic
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+#include "math.h"
+#include "stdbool.h"
+
+//led
+#include "led.h"
+
+//lcd
+#include "lcd.h"
+#include "Disp.h"
+
+//key
+#include "key.h"
+#include "gpio.h"
+
+//IC
+#include "IC.h"
+#include "tim.h"
+
+#endif
+
